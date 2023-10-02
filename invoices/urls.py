b15b22fc816f458c4f83cf936e60a26ff126b6b5
mukule <nelsonmasibo6@gmail.com>
2023-10-02 13:02:38 +0300

    removed the pandas thing

--- invoices/urls.py
+++ invoices/urls.py
@@ -7,8 +7,7 @@ app_name = 'invoices'
 urlpatterns = [
    path('', views.invoices, name='invoices'),
    path('invoices/<int:reservation_id>/', views.invoice_detail, name='invoice_detail'),
-   path('generate_invoice/<int:reservation_id>/', views.generate_invoice, name='generate_invoice'),
-   
+  
   
    
 ]
--- invoices/views.py
+++ invoices/views.py
@@ -7,7 +7,6 @@ from reportlab.lib.pagesizes import letter
 from django.shortcuts import get_object_or_404
 from reservations.models import Reservation
 import io
-from xhtml2pdf import pisa
 from django.db.models import Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
@@ -52,34 +51,3 @@ def invoice_detail(request, reservation_id):
     # Pass the reservation to the template context
     context = {'reservation': reservation}
     return render(request, 'invoices/invoice_detail.html', context)
-
-
-
-def generate_invoice(request, reservation_id):
-    # Get the specific reservation using reservation_id
-    reservation = get_object_or_404(Reservation, pk=reservation_id)
-
-    # Load the HTML template
-    template = get_template('invoices/invoice_template.html')
-    context = {'reservation': reservation}
-    html = template.render(context)
-
-    # Create a PDF buffer
-    pdf_buffer = io.BytesIO()
-
-    # Convert HTML to PDF using xhtml2pdf
-    pdf = pisa.CreatePDF(html, dest=pdf_buffer)
-
-    if not pdf.err:
-        # Get the PDF buffer's content and close it
-        pdf_content = pdf_buffer.getvalue()
-        pdf_buffer.close()
-
-        # Set the response to download the PDF
-        response = HttpResponse(content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename="invoice.pdf"'
-        response.write(pdf_content)
-
-        return response
-
-    return HttpResponse("Error generating PDF", status=500)
