743ecfccfd4bda3ac97cdc30f1a59aa9fab96e3f
mukule <nelsonmasibo6@gmail.com>
2024-02-21 12:58:06 +0300

    wed

--- inspection/views.py
+++ inspection/views.py
@@ -21,17 +21,36 @@ def handle_many_to_many_fields(inspection_instance, form):
     for field in ['emergency_equipment', 'oil_level', 'brake_fluid', 'power_steering_fluid', 'clutch_fluid', 'auto_transmission_fluid', 'radiator_fluid_level', 'windshield_washer_level', 'terminals_checked_and_tightened', 'battery_fluid', 'headlights_working', 'high_beam_working', 'brake_lights_working', 'indicators_working', 'reverse_lights_working', 'fog_lights_working', 'air_conditioning_working', 'radio_working', 'CD', 'USB', 'AUX', 'FM_Expander', 'windscreen_condition', 'seat_belts_functioning', 'electric_mirrors_functioning', 'electric_windows_functioning']:
         getattr(inspection_instance, field).set(form.cleaned_data[field])
 
-def handle_file_uploads(request, inspection_instance):
-    # Handle damage images
-    for image_file in request.FILES.getlist('car_damage_images'):
-        try:
-            DamageImage.objects.create(
-                inspection=inspection_instance,
-                d_image=image_file
-            )
-        except ValidationError as e:
-            # Handle validation error here, if needed
+from PIL import Image
+from io import BytesIO
+
+def handle_file_uploads(request, inspection_instance, max_file_size=10 * 1024 * 1024, max_width=800, max_height=600, max_quality=85):
+    for index, image_file in enumerate(request.FILES.getlist('car_damage_images')):
+        # Check file size
+        if image_file.size > max_file_size:
+            # Handle large file size error here
             pass
+        else:
+            try:
+                image = Image.open(image_file)
+                
+                # Check dimensions and reduce quality if necessary
+                if image.width > max_width or image.height > max_height:
+                    image.thumbnail((max_width, max_height), Image.ANTIALIAS)
+                
+                # Save the image to a BytesIO buffer with reduced quality
+                buffer = BytesIO()
+                image.save(buffer, format='JPEG', quality=max_quality)
+                
+                # Create DamageImage object with the optimized image
+                DamageImage.objects.create(
+                    inspection=inspection_instance,
+                    d_image=buffer,
+                )
+            except Exception as e:
+                # Handle any errors that may occur during image processing
+                pass
+
 
 def generate_and_send_email(inspection_instance, car_instance):
     # Create a PDF using ReportLab
